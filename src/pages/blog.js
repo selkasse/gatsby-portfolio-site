5fdc3eb5984e39c14055f69729ce1aff231058e5
selkasse <selkasse@gmu.edu>
2020-12-19 23:20:41 -0500

    initial commit

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>Blog</h1>
+      <p>This is the blog placeholder joint</p>
+    </Layout>
+  )
+}
---
+++ src/pages/projects.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Projects() {
+  return (
+    <Layout>
+      <h1>Projects</h1>
+      <p>This is the projects placeholder joint</p>
+    </Layout>
+  )
+}
