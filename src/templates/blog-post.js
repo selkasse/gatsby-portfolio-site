074ae26dbd3d39de014879c0eccc55f89b2a7485
selkasse <selkasse@gmu.edu>
2020-12-20 18:55:11 -0500

    get text working in blog post template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,12 +3,18 @@ import Layout from "../components/layout"
 
 export default function BlogPost({ data }) {
   const post = data.allSanityPost.edges[0].node
-  console.log(post._rawBody)
+  console.log(post._rawBody[0].children)
+  let bodyText = ""
+  post._rawBody[0].children.forEach(child => {
+    bodyText += child.text
+  })
+
+  console.log(bodyText)
 
   return (
     <Layout>
       <h1>{post.title}</h1>
-      {/* <p>{post.body.children.text}</p> */}
+      <p>{bodyText}</p>
     </Layout>
   )
 }
