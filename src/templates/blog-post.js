61f83ac5c0ba1234f07290741349d8699259db95
selkasse <selkasse@gmu.edu>
2020-12-20 17:33:46 -0500

    get graphQL query working for individul blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,10 +1,30 @@
 import React from "react"
 import Layout from "../components/layout"
 
-export default function BlogPost() {
+export default function BlogPost({ data }) {
+  const post = data.allSanityPost.edges[0].node
+  console.log(post._rawBody)
+
   return (
     <Layout>
-      <div>Hello blog post</div>
+      <h1>{post.title}</h1>
+      {/* <p>{post.body.children.text}</p> */}
     </Layout>
   )
 }
+
+export const query = graphql`
+  query($slug: String) {
+    allSanityPost(filter: { slug: { current: { eq: $slug } } }) {
+      edges {
+        node {
+          title
+          slug {
+            current
+          }
+          _rawBody
+        }
+      }
+    }
+  }
+`
